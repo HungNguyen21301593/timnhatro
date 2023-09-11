5ee0053be58e41fede00655fb9e53502afa17674
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-09-11 16:51:29 +0700

    add init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+free -h
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+apt  install docker-compose -f
