5d8bd849fd6155f99a9184c0f8e0d208cb2402a8
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-09-13 16:36:44 +0700

    update

--- index/src/app/components/main/main.component.ts
+++ index/src/app/components/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subscription } from 'rxjs';
 import { GeocodeResult } from 'src/app/interfaces/geocode-result';
 import { MapStateService } from 'src/app/services/map-state.service';
@@ -16,7 +17,7 @@ export class MainComponent implements OnInit, OnDestroy {
 
   public item!: GeocodeResult;
   subs: Subscription;
-  constructor(private mapstateService: MapStateService) {
+  constructor(private mapstateService: MapStateService, private snackBar: MatSnackBar) {
     this.subs = this.mapstateService.itemSelectedObservable.subscribe(item => {
       if (!item) {
         return;
@@ -25,12 +26,12 @@ export class MainComponent implements OnInit, OnDestroy {
       this.drawerHomeAddedItems?.open();
     })
   }
-  
+
   ngOnDestroy(): void {
     this.subs.unsubscribe();
   }
 
   ngOnInit() {
-
+    this.snackBar.open("Trải nghiệm tốt hơn bằng cách mở trong trình duyệt!", "OK", { duration: 2000 });
   }
 }
